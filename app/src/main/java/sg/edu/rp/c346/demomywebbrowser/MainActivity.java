82d774437b4c3f081456a6faaf5b1348d685105a
17038568 <17038568@myrp.edu.sg>
2019-07-30 11:59:27 +0800

    enhancement

--- app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLoadURL;
     WebView wvMyPage;
+    EditText etURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         wvMyPage = findViewById(R.id.webViewMyPage);
         btnLoadURL = findViewById(R.id.buttonLoad);
+        etURL = findViewById(R.id.etURL);
 
         wvMyPage.setWebViewClient(new WebViewClient());
-
-
 
         btnLoadURL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "https://www.facebook.com/";
+                //String url = "https://www.facebook.com/";
+                String url = etURL.getText().toString();
                 wvMyPage.loadUrl(url);
+
+                //Disable file access within WebView
                 wvMyPage.getSettings().setAllowFileAccess(false);
+                //Tell the WebView to enable JavaScript execution
                 wvMyPage.getSettings().setJavaScriptEnabled(true);
+                //Enable the built-in zoom control for the WebView
                 wvMyPage.getSettings().setBuiltInZoomControls(true);
+
+                //set the EditText to become invisible
+                etURL.setVisibility(View.GONE);
 
 
             }
