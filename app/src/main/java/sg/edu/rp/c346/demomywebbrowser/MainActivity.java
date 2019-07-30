38d57d0c38d13bece0ae78a06a22fd9be2e7b0c7
17038568 <17038568@myrp.edu.sg>
2019-07-30 11:40:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.demomywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        btnLoadURL = findViewById(R.id.buttonLoad);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.facebook.com/";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
